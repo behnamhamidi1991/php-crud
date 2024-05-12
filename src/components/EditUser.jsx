69d7914c33ba27dc073933199c67043f5ae05a8f
Behnam Hamidi <eslp91@hotmail.com>
2024-05-13 00:59:04 +0330

    update

---
+++ src/components/EditUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditUser = () => {
+  return (
+    <div className="component">
+      <h2>Edit User</h2>
+    </div>
+  );
+};
+
+export default EditUser;
---
+++ src/components/ListUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListUser = () => {
+  return (
+    <div className="components">
+      <h2>List User</h2>
+    </div>
+  );
+};
+
+export default ListUser;
