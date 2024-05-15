4b3e21b0999d879905dd4efb524e80e176053aef
Behnam Hamidi <eslp91@hotmail.com>
2024-05-15 12:26:08 +0330

    go

--- index.php
+++ index.php
@@ -1,74 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous"
-    />
-    <title>Crud Application</title>
-  </head>
-  <body>
-    <div class="container my-5">
-      <h2>List of Clients</h2>
-      <a href="/myshop/create.php" role="button">New Client</a>
 
-      <table class="table">
-        <thead>
-          <?php
-            $servername = "localhost";
-            $username = "behnam";
-            $password = "123456";
-            $database = "myshop";
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+  <title>Crud Application</title>
+</head>
 
-            // Create a connection
-            $connection = new mysqli($servername, $username, $password, $database);
+<body>
+  <div class="container my-5">
+    <h2>List of Clients</h2>
+    <a href="/myshop/create.php" role="button">New Client</a>
 
-            // Check the connection
-            if ($connection->connect_error) {
-              die("Connection Failed!" . $connection->connect_error);
-            }
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Address</th>
+          <th>Create At</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
 
-            // Read all row from database table
-            $sql = "SELECT * FROM clients";
-            $results = $connection->query($sql);
+        <?php
+        $servername = "localhost";
+        $username = "behnam";
+        $password = "123456";
+        $database = "myshop";
 
-            if (!$results) {
-              die("Invalid query: " .$connection->error);
-            }
+        // Create a connection
+        $connection = new mysqli($servername, $username, $password, $database);
 
-            // Read data of each row
-            while($row = $result->fetch_assoc()) {
-              
-            }
+        // Check the connection
+        if ($connection->connect_error) {
+          die("Connection Failed!" . $connection->connect_error);
+        }
 
-          ?>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Address</th>
-            <th>Create At</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <td>10</td>
-          <td>Bill Gates</td>
-          <td>bill.gates@microsoft.com</td>
-          <td>+987654321</td>
-          <td>New York, USA</td>
-          <td>18/05/2022</td>
-          <td>
-            <a href="/" class="btn btn-primary btn-sm">Edit</a>
-            <a href="/" class="btn btn-danger btn-sm">Delete</a>
-          </td>
-        </tbody>
-      </table>
-    </div>
-  </body>
+        // Read all row from database table
+        $sql = "SELECT * FROM clients";
+        $results = $connection->query($sql);
+
+        if (!$results) {
+          die("Invalid query: " . $connection->error);
+        }
+
+        // Read data of each row
+        while ($row = $result->fetch_assoc()) {
+          echo "";
+        }
+
+        ?>
+
+        <td>10</td>
+        <td>Bill Gates</td>
+        <td>bill.gates@microsoft.com</td>
+        <td>+987654321</td>
+        <td>New York, USA</td>
+        <td>18/05/2022</td>
+        <td>
+          <a href="/" class="btn btn-primary btn-sm">Edit</a>
+          <a href="/" class="btn btn-danger btn-sm">Delete</a>
+        </td>
+      </tbody>
+    </table>
+  </div>
+</body>
+
 </html>
