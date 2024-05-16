56eb630a57188ab19bc1e7484ddf05facd41052b
Behnam Hamidi <eslp91@hotmail.com>
2024-05-17 00:29:30 +0330

    Refactoring

--- controllers/error/404.php
+++ controllers/error/404.php
@@ -3,18 +3,6 @@
 <?php loadPartial('showcase-search'); ?>
 <?php loadPartial('top-banner'); ?>
 
-
-
-<!-- Top Banner -->
-<section class="bg-blue-900 text-white py-6 text-center">
-    <div class="container mx-auto">
-        <h2 class="text-3xl font-semibold">Unlock Your Career Potential</h2>
-        <p class="text-lg mt-2">
-            Discover the perfect job opportunity for you.
-        </p>
-    </div>
-</section>
-
 <section>
     <div class="container mx-auto p-4 mt-4">
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">404 Error</div>
