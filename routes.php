9fb9340ed1de52c89f023779c968a4eafcf7c290
Behnam Hamidi <eslp91@hotmail.com>
2024-05-17 01:47:01 +0330

    Upload

--- routes.php
+++ routes.php
@@ -8,6 +8,7 @@
 //     '404' => 'controllers/error/404.php'
 // ];
 
+// FIRST: URI ||| SECOND: CONTROLLER
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
